550fb77ce584cc6fb2fd09551406dd6eaa095c8d
zhuwenjie11 <48839949@qq.com>
2016-08-09 16:01:15 +0800

    这是一个测试

---
+++ src/main/java/com/eastcom/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.eastcom.git;
+
+import java.util.Date;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("hello git,time:"+new Date());
+	}
+
+}
