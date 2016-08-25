83907841159751fbd5982e2ff4cf8b2696a6e353
zhuwenjie11 <48839949@qq.com>
2016-08-25 09:53:25 +0800

    添加web支持

--- src/main/java/com/eastcom/git/HelloGit.java
+++ src/main/java/com/eastcom/git/HelloGit.java
@@ -2,10 +2,36 @@ package com.eastcom.git;
 
 import java.util.Date;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created on 19/11/14.
+ */
+
+@Controller
+@EnableWebMvc
+@EnableAutoConfiguration
 public class HelloGit {
 
-	public static void main(String[] args) {
-		System.out.println("hello222 git,time:"+new Date());
-	}
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World: Spring-boot Sample Application using Maven and Docker , time : "+new Date();
+    }
+    
+    public HelloGit(){
+    	System.out.println("init bean HelloGit!");
+    }
+
+    public static void main(String[] args) throws Exception {
+    	System.out.println("start app!");
+        SpringApplication.run(HelloGit.class, args);
+    }
 
 }
