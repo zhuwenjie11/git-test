f98c5514444917778ced19cac88f8d6172e60fa1
zhuwenjie11 <48839949@qq.com>
2016-09-07 14:43:40 +0800

    修改描述55

--- src/main/java/com/eastcom/git/HelloGit.java
+++ src/main/java/com/eastcom/git/HelloGit.java
@@ -29,7 +29,7 @@ public class HelloGit extends SpringBootServletInitializer implements EmbeddedSe
     }
     
     public HelloGit(){
-    	System.out.println("init bean HelloGit!");
+    	System.out.println("init bean HelloGit 44!");
     }
 
     public static void main(String[] args) throws Exception {
